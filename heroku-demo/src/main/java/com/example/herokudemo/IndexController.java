5afc988525c254e07227dbdd1834711fff9007ed
lfelikson <lfelikson@gmail.com>
2020-08-12 18:00:21 -0400

    first commit

---
+++ heroku-demo/src/main/java/com/example/herokudemo/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.herokudemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class IndexController {
+
+   @GetMapping("/")
+
+   public String index() {
+       return "Hello there! I am working on Heroku, Leonid";
+
+   }
+
+}
