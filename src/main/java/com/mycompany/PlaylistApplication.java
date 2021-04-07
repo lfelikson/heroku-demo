6c1251a663f59726f1bb2e1ec339b9882fd01d61
lfelikson <lfelikson@gmail.com>
2021-04-06 20:16:35 -0400

    my april commit

---
+++ src/main/java/com/mycompany/PlaylistApplication.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistApplication {
+
+	public static void main(String[] args) {
+		System.out.println("++++++++++++++++*********************");
+		SpringApplication.run(PlaylistApplication.class, args);
+	}
+
+}
